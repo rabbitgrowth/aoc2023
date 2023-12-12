1140448fcef48dca2303fb625e4e3edc4e333d69
Nick Yeow <ytknick@gmail.com>
2023-12-13 00:40:12 +0800

    Memoize

--- 12.py
+++ 12.py
@@ -1,25 +1,29 @@
-invalid = set()
+memo = {}
 
 def valid(string, lengths, prev=None):
-    if (string, tuple(lengths), prev) in invalid:
-        return False
+    key = (string, tuple(lengths), prev)
+    if key in memo:
+        return memo[key]
     if not string:
-        return lengths == [0] or not lengths # enough #-groups?
-    if string[0] == '.':
-        if prev == '#':
-            if lengths[0]:
-                invalid.add((string, tuple(lengths), prev))
-                return False
-            return valid(string[1:], lengths[1:], string[0])
-        else:
-            return valid(string[1:], lengths, string[0])
-    elif string[0] == '#':
-        if not lengths: # too many #-groups
-            invalid.add((string, tuple(lengths), prev))
-            return False
-        return valid(string[1:], [lengths[0] - 1, *lengths[1:]], string[0])
-    elif string[0] == '?':
-        return sum(valid(char + string[1:], lengths, prev) for char in '.#')
+        ret = lengths == [0] or not lengths # enough #-groups?
+    else:
+        if string[0] == '.':
+            if prev == '#':
+                if lengths[0]:
+                    ret = False
+                else:
+                    ret = valid(string[1:], lengths[1:], string[0])
+            else:
+                ret = valid(string[1:], lengths, string[0])
+        elif string[0] == '#':
+            if not lengths: # too many #-groups
+                ret = False
+            else:
+                ret = valid(string[1:], [lengths[0] - 1, *lengths[1:]], string[0])
+        elif string[0] == '?':
+            ret = sum(valid(char + string[1:], lengths, prev) for char in '.#')
+    memo[key] = ret
+    return ret
 
 with open('12.txt') as f:
     total = 0
